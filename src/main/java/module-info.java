124bc85d73d5c6e9c51b91f5402dc54a589c9786
Nina Madra <ninamadra@gmail.com>
2022-10-14 20:02:44 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.tictactoe {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.example.tictactoe to javafx.fxml;
+    exports com.example.tictactoe;
+}
